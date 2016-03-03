965072292e1df8e83835761094d6f24c1a6e5737
Rick Houser <21@p6-4.hbtn.io>
2016-03-02 16:48:41 -0800

    add comments to js

--- post_a_status.js
+++ post_a_status.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() { 
 	document.getElementById("poststatusbtn").addEventListener("click", postpre);
-	function postpre() {
+	function postpre() { // call toggle function on poststatusbtn
 		var elem = document.getElementById('statuspost');
-		elem.toggle();
+		elem.toggle(); // call toggle on div containing new status post
 		var elem = document.getElementById('poststatusbtn');
 		elem.toggle();
 	}
 	document.getElementById("postbtn").addEventListener("click", postpostbtn);
-	function postpostbtn() {
+	function postpostbtn() { // call toggle function on reply button
 		var elem = document.getElementById('statuspost');
-		elem.toggle();
+		elem.toggle(); // call toggle on div containing status reply
 		var elem = document.getElementById('poststatusbtn');
 		elem.toggle();
 	}
--- reply.js
+++ reply.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-	var reply = document.querySelectorAll('#reply');
-	var post_button = document.querySelectorAll('#post');
-	var r = 0, r_length = reply.length;
+	var reply = document.querySelectorAll('#reply'); // find all elements with reply id
+	var post_button = document.querySelectorAll('#post'); // same as above for post id
+	var r = 0, r_length = reply.length; // create & set new vars to 0 and to number of reply btns (length)
 	var p = 0, p_length = post.length;
-		for (r; r < r_length; r++) {
-		reply[r].addEventListener('click', function() {
-		var parent = this.closest('#ask-post-status');
-		var post = this.parentNode.parentNode.parentNode.nextElementSibling;
-		post.toggle();
+		for (r; r < r_length; r++) { // iterates through all reply btns adding event listeners to each
+		reply[r].addEventListener('click', function() { // when click is heard do the following:
+		var parent = this.closest('#ask-post-status');  // set target 1 for toggling
+		var post = this.parentNode.parentNode.parentNode.nextElementSibling; // set target 2 for toggle
+		post.toggle(); 
 		parent.toggle();
-				parent.classList.add('hidden');
 		});
 	}
-	for (p; p < p_length; p++) {	
+	for (p; p < p_length; p++) { // does same as above but for forms containing replies to status updates
 		post_button[p].addEventListener('click', function() {
 		var post_box = this.closest('#reply_post');
 		post_box.toggle();
