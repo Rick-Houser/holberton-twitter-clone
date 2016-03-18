6a2521663c375119e9143c99c21fa587eba4bcb3
Chandler Mayo <chandler@chandlermayo.com>
2016-03-18 13:36:48 -0700

    Small tweek to fix a potential issue

--- load_more.js
+++ load_more.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("showmore").addEventListener("click", showmoreposts);
 	function showmoreposts() { 
     	ajaxGet("/statuses-1.html", function(response) {
-        document.getElementById("show-more-status").innerHTML = xmlhttp.responseText;
+        document.getElementById("show-more-status").innerHTML = response;
         document.getElementById("show-more-status").style.backgroundColor = '#FFFFFF';
         });
 	}
