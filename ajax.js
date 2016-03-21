cd6ee87f129649872a405537c77702fc9b01303c
Rick Houser <21@p6-4.hbtn.io>
2016-03-21 15:43:40 -0700

    add script to wait 2 seconds before executing AJAX via setTimeout

--- ajax.js
+++ ajax.js
@@ -1,12 +1,12 @@
-function ajaxGet(url, onSuccess) { 
-		xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function()
-        {
-            if(xmlhttp.readyState == 4 && xmlhttp.status == 200)
-            {
-                onSuccess(xmlhttp.responseText);   
-            }
-        }
-        xmlhttp.open('GET', url, true);
-        xmlhttp.send();
-    }
+function ajaxGet(url, onSuccess) {
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = setTimeout(function()
+	{
+	    if(xmlhttp.readyState == 4 && xmlhttp.status == 200)
+	    {
+				onSuccess(xmlhttp.responseText);
+	    }
+	}, 2000)
+	xmlhttp.open('GET', url, true);
+	xmlhttp.send();
+}
---
+++ disable_me.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded", function() {
+// 	document.getElementById("showmore").addEventListener("click", disable);
+// 	function disable() {
+// 		HTMLElement.prototype.toggle = function() {
+// 	  if (this.classList.contains("hidden")) { // if element is hidden, remove hidden.
+// 	    this.classList.remove('hidden');
+// 	  } else {
+// 	    this.classList.add('hidden'); // else add hidden
+// 	  }
+// 	}
+// });
