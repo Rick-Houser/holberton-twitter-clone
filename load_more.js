e3c5bdac1f078d107b423f41d8f1fac164b9ee62
Rick Houser <21@p6-4.hbtn.io>
2016-03-21 16:59:57 -0700

    remove unnecessary comment

--- load_more.js
+++ load_more.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("showmore").addEventListener("click", showmoreposts);
 	function showmoreposts() {
 		{
-			// document.getElementById("showmore").href = "#/";
     	ajaxGet("/statuses-1.html", function(response) {
         document.getElementById("show-more-status").innerHTML = response;
         document.getElementById("show-more-status").style.backgroundColor = '#FFFFFF';
