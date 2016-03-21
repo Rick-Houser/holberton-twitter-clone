3497d56809eca70ae00a42015c3f9defaa05f064
Rick Houser <21@p6-4.hbtn.io>
2016-03-21 16:56:50 -0700

    change button on click

--- ajax.js
+++ ajax.js
@@ -1,5 +1,6 @@
 function ajaxGet(url, onSuccess) {
 	xmlhttp = new XMLHttpRequest();
+	document.getElementById("show-more-status").innerHTML = "...processing...";
 	xmlhttp.onreadystatechange = setTimeout(function()
 	{
 	    if(xmlhttp.readyState == 4 && xmlhttp.status == 200)
--- load_more.js
+++ load_more.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById("showmore").addEventListener("click", showmoreposts);
-	document.getElementById("showmore").href = "#"
 	function showmoreposts() {
+		{
+			// document.getElementById("showmore").href = "#/";
     	ajaxGet("/statuses-1.html", function(response) {
         document.getElementById("show-more-status").innerHTML = response;
         document.getElementById("show-more-status").style.backgroundColor = '#FFFFFF';
-        });
+      });
+		}
 	}
 });
