786fb6edd895d66eee89399b924cb96780583bd4
FrackenToaster <frakentoaster@gmail.com>
2016-04-11 22:23:09 -0700

    add function to check for existing users upon login

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+$users = [
+    array("id" => 1, "login" => "user1", "password" => "password1", "full_name" => "User 1"),
+    array("id" => 2, "login" => "user2", "password" => "password2", "full_name" => "User 2"),
+    array("id" => 3, "login" => "user3", "password" => "password3", "full_name" => "User 3"),
+  ];
+
+function userExists($login, $password, $users) {
+	foreach ($$users as $user) {
+		if ($user["login"] == $login && $user["password"] == $password) {
+			return $user;
+		}
+		echo "InvalidArgumentException";
+		return false;
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
